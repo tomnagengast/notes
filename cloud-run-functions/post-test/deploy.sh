eacea0703c2480c4a1a498044c680a65c06c139a
tomnagengast <tnagengast@gmail.com>
2024-11-17 13:05:47 -0800

    Clean up test

--- cloud-run-functions/post-test/deploy.sh
+++
@@ -1,7 +0,0 @@
-SERVICE_NAME=$(basename $(pwd))
-
-gcloud beta run deploy $SERVICE_NAME \
-      --source . \
-      --function helloHttp \
-      --region us-central1 \
-      --no-allow-unauthenticated
